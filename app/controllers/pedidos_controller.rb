d304ac140eaf5225d9be9884c317e7cb3c45dad8
andre <andre.barbosa@mulheres.ce.gov.br>
2026-01-21 14:04:33 -0300

    soma dos pedidos

--- app/controllers/pedidos_controller.rb
+++ app/controllers/pedidos_controller.rb
@@ -1,84 +1,52 @@
 class PedidosController < ApplicationController
-  before_action :set_pedido, only: %i[ show edit update destroy ]
-
-  # GET /pedidos
+  # Corrigindo o erro do index: agora buscamos todos os pedidos
   def index
-    # Usamos o .includes(:customer) para carregar o nome do cliente mais rápido (otimização PostgreSQL)
     @pedidos = Pedido.all.includes(:customer)
   end
 
-  # GET /pedidos/1
-  def show
-    # Aqui você pode ver os sabores incluídos no pedido
-    @itens = @pedido.pedido_items.includes(:product)
-  end
-
-  # GET /pedidos/new
   def new
-    @pedido = Pedido.new
-    # Isso garante que o formulário saiba que pode receber itens
-    @pedido.pedido_items.build
+    if params[:pedido]
+      @pedido = Pedido.new(pedido_params)
+      
+      # Captura os IDs dos sabores marcados
+      ids = params[:pedido][:pedido_items_attributes]&.map { |i| i[:product_id] }&.reject(&:blank?)
+      
+      if ids.present?
+        # Ruby faz o cálculo
+        subtotal = Product.where(id: ids).sum(:price)
+        desconto = subtotal * (@pedido.discount_percent.to_f / 100.0)
+        @pedido.total_price = subtotal - desconto
+        
+        # Mantém os itens marcados na tela após o recarregamento
+        @pedido.product_ids = ids
+      end
+    else
+      @pedido = Pedido.new
+    end
   end
 
-  # GET /pedidos/1/edit
-  def edit
-  end
-
-  # POST /pedidos
   def create
     @pedido = Pedido.new(pedido_params)
+    # Lógica de segurança para garantir o preço antes de salvar
+    ids = params[:pedido][:pedido_items_attributes]&.map { |i| i[:product_id] }&.reject(&:blank?)
+    subtotal = Product.where(id: ids).sum(:price)
+    @pedido.total_price = subtotal - (subtotal * (@pedido.discount_percent.to_f / 100.0))
 
-    respond_to do |format|
-      if @pedido.save
-        format.html { redirect_to @pedido, notice: "Pedido criado com sucesso!" }
-        format.json { render :show, status: :created, location: @pedido }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @pedido.errors, status: :unprocessable_entity }
-      end
+    if @pedido.save
+      redirect_to @pedido, notice: "Pedido realizado com sucesso!"
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /pedidos/1
-  def update
-    respond_to do |format|
-      if @pedido.update(pedido_params)
-        format.html { redirect_to @pedido, notice: "Pedido atualizado com sucesso!", status: :see_other }
-        format.json { render :show, status: :ok, location: @pedido }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @pedido.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /pedidos/1
-  def destroy
-    @pedido.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to pedidos_path, notice: "Pedido excluído.", status: :see_other }
-      format.json { head :no_content }
-    end
+  def show
+    @pedido = Pedido.find(params[:id])
   end
 
   private
 
-  # Use callbacks para compartilhar configuração comum entre as ações.
-  def set_pedido
-    # Ajustado para usar params[:id] que é o padrão mais estável
-    @pedido = Pedido.find(params[:id])
-  end
-
-  # O AJUSTE MAIS IMPORTANTE: Adicionando os atributos aninhados (sabores)
   def pedido_params
-    params.require(:pedido).permit(
-      :customer_id,
-      :total_price,
-      :discount_percent,
-      :status,
-      :delivery_address,
-      pedido_items_attributes: [ :id, :product_id, :quantity, :_destroy ]
-    )
+    params.require(:pedido).permit(:customer_id, :delivery_address, :discount_percent, :total_price,
+                                   pedido_items_attributes: [:product_id])
   end
 end
--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -2,7 +2,12 @@ class Pedido < ApplicationRecord
   belongs_to :customer
   has_many :pedido_items, dependent: :destroy
   has_many :products, through: :pedido_items
-
-  # Essencial para o controller aceitar os sabores
   accepts_nested_attributes_for :pedido_items, allow_destroy: true
+
+  def calcular_total_pelo_ruby
+    # Soma o preço dos produtos associados no banco de dados
+    subtotal = products.sum(:price)
+    desconto = subtotal * (discount_percent.to_f / 100.0)
+    self.total_price = subtotal - desconto
+  end
 end
